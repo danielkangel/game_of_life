550bcd161b7a6462aedfe87d503741df72c3feb3
Daniel Angel <70451728+danielkangel@users.noreply.github.com>
2020-12-21 18:20:35 -0600

    demo of board generation and rendering

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import board as b
 
-board1 = b.Board(50, 50)
-board1.random_state()
-board1.render()
+board_1 = b.Board(50, 50)
+board_1.random_state()
+board_1.render()
 
+print()
 
+board_2 = b.Board(10, 10)
+board_2.render()
+
+print()
+
+board_3 = b.Board(30, 10)
+board_3.random_state(0.1)
+board_3.render()
