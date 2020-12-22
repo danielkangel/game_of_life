6e3a97c766acfd7868b77702c87a6f93e0e157a5
Daniel Angel <dka190000@utdallas.edu>
2020-12-22 11:09:29 -0600

    Update board.py
    added the next_state() method

--- board.py
+++ board.py
@@ -2,38 +2,35 @@ import random as rand
 
 
 class Board:
-    def __init__(self, width, height):
+    def __init__(self, width=10, height=10):
         """Creates a blank 2D array based on the passed width and height"""
-        blank_row = [0] * width
         self.width = width
         self.height = height
-        self.board = [blank_row for num in range(0, height)]
+        self.board = [[0 for val in range(0, width)] for val in range(0, height)]
 
     def random_state(self, rand_control=0.5):
         """Updates the board with a new 2D array of cells"""
 
         # Populate the board with random amount of alive cells
-        for index in range(0, self.height):
-            cells = []
-            for num in range(0, self.width):
+        for row in range(0, self.height):
+            for index in range(0, self.width):
                 random_number = rand.random()
                 if random_number >= rand_control:
                     cell_state = 0
                 else:
                     cell_state = 1
-                cells.append(cell_state)
-            self.board[index] = cells
+                self.board[row][index] = cell_state
 
     def render(self):
         """Formats the board state and prints it to the console"""
         formatted_board = ""
-        for num in range(0, self.width + 2):
+        for num in range(0, (self.width + 1) + 1):
             formatted_board += '-'
         for row in self.board:
             formatted_board += '\n|'
             for num in range(0, self.width):
                 if row[num]:
-                    formatted_board += '#'
+                    formatted_board += '\u25A0'
                 else:
                     formatted_board += ' '
             formatted_board += '|'
@@ -41,3 +38,39 @@ class Board:
         for num in range(0, self.width + 2):
             formatted_board += '-'
         print(formatted_board)
+
+    def check_cell(self, location):
+        """Scans surrounding cells to check whether or not the given cell should die or not"""
+        row = location[0]
+        index = location[1]
+        surrounding_cells = 0
+
+        # Loops through each surrounding element and checks the amount of surrounding living cells
+        for scan_row in range((row - 1), (row + 1) + 1):
+            for scan_index in range((index - 1), (index + 1) + 1):
+                if (scan_index == index) and (scan_row == row): continue
+                if self.board[scan_row % self.height][scan_index % self.width] == 1:
+                    surrounding_cells += 1
+
+        # Returns the correct status based on the surrounding cells
+        if self.board[row][index] == 1:
+            if surrounding_cells < 2:
+                return 0
+            elif surrounding_cells <= 3:
+                return 1
+            else:
+                return 0
+        else:
+            if surrounding_cells == 3:
+                return 1
+            else:
+                return 0
+
+    def next_state(self):
+        """Checks each element and updates the board"""
+        new_board = [[0 for x in range(0, self.width)] for y in range(0, self.height)]
+        for row in range(0, self.height):
+            for index in range(0, self.width):
+                if self.check_cell((row, index)):
+                    new_board[row][index] = 1
+        self.board = new_board[:][:]
