389e5d93563e227340d8af8ebf863515c25cbf07
Daniel Angel <dka190000@utdallas.edu>
2021-05-28 20:11:35 -0500

    implemented a GUI using PyGame

--- board.py
+++ board.py
@@ -1,4 +1,9 @@
 import random as rand
+import pygame
+
+# set the alive and dead cell colors
+alive_cell = (0, 0, 0)
+dead_cell = (236, 236, 236)
 
 
 class Board:
@@ -6,7 +11,7 @@ class Board:
         """Creates a blank 2D array based on the passed width and height"""
         self.width = width
         self.height = height
-        self.board = [[0 for val in range(0, width)] for val in range(0, height)]
+        self.board = [[dead_cell for val in range(0, width)] for val in range(0, height)]
 
     def random_state(self, rand_control=0.5):
         """Updates the board with a new 2D array of cells"""
@@ -16,28 +21,20 @@ class Board:
             for index in range(0, self.width):
                 random_number = rand.random()
                 if random_number >= rand_control:
-                    cell_state = 0
+                    cell_state = dead_cell
                 else:
-                    cell_state = 1
+                    cell_state = alive_cell
                 self.board[row][index] = cell_state
 
-    def render(self):
+    def render(self, surface, cell_size):
         """Formats the board state and prints it to the console"""
-        formatted_board = ""
-        for num in range(0, (self.width + 1) + 1):
-            formatted_board += '-'
-        for row in self.board:
-            formatted_board += '\n|'
-            for num in range(0, self.width):
-                if row[num]:
-                    formatted_board += '\u25A0'
-                else:
-                    formatted_board += ' '
-            formatted_board += '|'
-        formatted_board += '\n'
-        for num in range(0, self.width + 2):
-            formatted_board += '-'
-        print(formatted_board)
+        y = 0
+        for r in self.board:
+            x = 0
+            for c in r:
+                pygame.draw.rect(surface, c, (x, y+2*cell_size, cell_size - 1, cell_size - 1))
+                x += cell_size
+            y += cell_size
 
     def check_cell(self, location):
         """Scans surrounding cells to check whether or not the given cell should die or not"""
@@ -48,12 +45,18 @@ class Board:
         # Loops through each surrounding element and checks the amount of surrounding living cells
         for scan_row in range((row - 1), (row + 1) + 1):
             for scan_index in range((index - 1), (index + 1) + 1):
-                if (scan_index == index) and (scan_row == row): continue
-                if self.board[scan_row % self.height][scan_index % self.width] == 1:
+                if (scan_index == index) and (scan_row == row):
+                    continue
+                if (scan_row >= self.height) or (scan_row < 0):
+                    continue
+                if (scan_index >= self.width) or (scan_index < 0):
+                    continue
+
+                if self.board[scan_row][scan_index] == alive_cell:
                     surrounding_cells += 1
 
         # Returns the correct status based on the surrounding cells
-        if self.board[row][index] == 1:
+        if self.board[row][index] == alive_cell:
             if surrounding_cells < 2:
                 return 0
             elif surrounding_cells <= 3:
@@ -68,9 +71,18 @@ class Board:
 
     def next_state(self):
         """Checks each element and updates the board"""
-        new_board = [[0 for x in range(0, self.width)] for y in range(0, self.height)]
+        new_board = [[dead_cell for x in range(0, self.width)] for y in range(0, self.height)]
         for row in range(0, self.height):
             for index in range(0, self.width):
                 if self.check_cell((row, index)):
-                    new_board[row][index] = 1
+                    new_board[row][index] = alive_cell
         self.board = new_board[:][:]
+
+    def clear_board(self):
+        self.board = [[dead_cell for val in range(0, self.width)] for val in range(0, self.height)]
+
+    def swap_cell(self, x, y):
+        if self.board[y][x] == alive_cell:
+            self.board[y][x] = dead_cell
+        else:
+            self.board[y][x] = alive_cell
--- main.py
+++ main.py
@@ -1,11 +1,142 @@
 import board as b
-from os import system
 from time import sleep
+import pygame
 
-board_1 = b.Board(50, 25)
-board_1.random_state()
+# set the window dimensions
+x_size = 30
+y_size = 30
+cell_size = 20
+
+# set the initial update speed and pause/unpause flag
+speed = 0.1
+next_state = 1
+
+# set the background color and the path for the icons
+background_color = (185, 185, 185)
+path = "./assets/icons/"
+
+# initialize the window
+pygame.init()
+surface = pygame.display.set_mode((x_size * cell_size, y_size * cell_size + 2 * cell_size))
+surface.fill(background_color)
+pygame.display.set_caption("Conway's Game of Life")
+
+# initialize the font for the speed
+cour = pygame.font.Font("./assets/font/cour.ttf", 24)
+
+# create a new board object
+board_1 = b.Board(x_size, y_size)
+
+# load all the images into pygame surfaces
+new_img = pygame.image.load(path + "clear_board.png")
+pause1_img = pygame.image.load(path + "pause_unclicked.png")
+pause2_img = pygame.image.load(path + "pause_clicked.png")
+shuffle_img = pygame.image.load(path + "random_board.png")
+slow_img = pygame.image.load(path + "slow_down.png")
+fast_img = pygame.image.load(path + "speed_up.png")
+logo = pygame.image.load(path + "logo.png")
+
+# set the logo for the window
+pygame.display.set_icon(logo)
+
+# scale each of the images to the correct size
+new_img = pygame.transform.scale(new_img, (2 * cell_size, 2 * cell_size))
+pause1_img = pygame.transform.scale(pause1_img, (2 * cell_size, 2 * cell_size))
+pause2_img = pygame.transform.scale(pause2_img, (2 * cell_size, 2 * cell_size))
+shuffle_img = pygame.transform.scale(shuffle_img, (2 * cell_size, 2 * cell_size))
+slow_img = pygame.transform.scale(slow_img, (2 * cell_size, 2 * cell_size))
+fast_img = pygame.transform.scale(fast_img, (2 * cell_size, 2 * cell_size))
+
+
+# render the board and update the window
+def redisplay(board):
+    board.render(surface, cell_size)
+    pygame.display.update()
+
+
+# display all the buttons except the pause button
+def display_buttons(screen):
+    screen.blit(new_img, (0, 0))
+    screen.blit(shuffle_img, (4 * cell_size, 0))
+    screen.blit(slow_img, (6 * cell_size, 0))
+    screen.blit(fast_img, (8 * cell_size, 0))
+
+
+# get the index of the mouse position
+def get_cell_idx(place):
+    x, y = place
+    y -= 2 * cell_size
+    x /= cell_size
+    y /= cell_size
+    return int(x), int(y)
+
+
+# loop until the window is exited
 while True:
-    board_1.render()
-    board_1.next_state()
-    sleep(0.1)
-    system('cls')
+    swap_cells = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
+
+        elif event.type == pygame.KEYDOWN:
+            pressed = pygame.key.get_pressed()
+
+            if pressed[pygame.K_SPACE]:
+                next_state = -next_state + 1
+
+            if pressed[pygame.K_LEFT]:
+                if speed < 0.3:
+                    speed += 0.025
+
+            if pressed[pygame.K_RIGHT]:
+                if speed > 0.05:
+                    speed -= 0.025
+
+            if pressed[pygame.K_n]:
+                board_1.clear_board()
+
+            if pressed[pygame.K_r]:
+                board_1.random_state()
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # locate where user pressed and update that one cell
+            loc = pygame.mouse.get_pos()
+            if loc[-1] > 2 * cell_size:
+                x, y = get_cell_idx(loc)
+                board_1.swap_cell(x, y)
+                swap_cells = True
+
+            else:
+                x_pos = loc[0]
+                if x_pos < 2 * cell_size:
+                    board_1.clear_board()
+
+                elif x_pos < 4 * cell_size:
+                    next_state = -next_state + 1
+
+                elif x_pos < 6 * cell_size:
+                    board_1.random_state()
+
+                elif x_pos < 8 * cell_size:
+                    if speed < 0.2:
+                        speed += 0.025
+
+                elif x_pos < 10 * cell_size:
+                    if speed > 0.05:
+                        speed -= 0.025
+
+    text = cour.render(f"x{round(0.1 / speed, 1)}", True, (0, 0, 0))
+    surface.fill(background_color)
+
+    if next_state:
+        if not swap_cells:
+            board_1.next_state()
+        surface.blit(pause1_img, (2 * cell_size, 0))
+        sleep(speed)
+    else:
+        surface.blit(pause2_img, (2 * cell_size, 0))
+
+    display_buttons(surface)
+    surface.blit(text, ((x_size - 3) * cell_size, 0.5 * cell_size))
+    redisplay(board_1)
